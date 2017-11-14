1aeaee321952e57b3d8236a60643387c120deda0
Loki2099 <33090971+Loki2099@users.noreply.github.com>
2017-11-13 21:45:37 -0600

    Added search to inventory

--- FEFProject/scripts/inventScript.js
+++ FEFProject/scripts/inventScript.js
@@ -22,8 +22,57 @@ function pgLoaded(){
 
 	var html = "";
 	for(var i = 0; i < cars.length; i++) {
-		html += "<tr><td>" + cars[i].make + "</td><td>" + cars[i].model + "</td><td>" + cars[i].year + "</td><td>" + cars[i].price + "</td><td>" + cars[i].tax + "</td><td>" + cars[i].total + "</td></tr>";
+		html += "<tr name=\"cars\"><td>" + cars[i].make + "</td><td>" + cars[i].model + "</td><td>" + cars[i].year + "</td><td>$" + cars[i].price + "</td><td>$" + cars[i].tax + "</td><td>$" + cars[i].total + "</td></tr>";
 	}
 
 	$("#inventTable tbody").append(html);
+
+	$("#inventSearch").keyup(function(){
+		var sbVal = $("#inventSearch").val().toLowerCase();
+		if(sbVal === ""){removeAll()}
+		else{filterStart(sbVal);}
+	});
+
+	$("#inventSearch").focusout(function(){
+		if($("#inventSearch").val() === ""){removeAll();}
+	});
+
+	function filterStart(sbVal){
+		var count = 0;
+		var hide = valueTesting(sbVal);
+		var tblrow = $("tr[name = 'cars']");
+		for(var i = 0; i < hide.length; i++) {
+			if(hide[i] === false){
+				count++;
+				if($(tblrow[i]).hasClass("hidden")){break}
+				else{$(tblrow[i]).addClass("hidden");}
+			}
+			else{removeHide(tblrow[i]);}
+		};
+		if(count === hide.length) { alert("Notice, no results found for current search value."); }
+	};
+
+	function valueTesting(sbVal){
+		var hide = Array(cars.length);
+		for (var i = 0; i < cars.length; i++) {
+			var found = false;
+			var car = cars[i];
+			if(car.make.toLowerCase().search(sbVal) > -1){found = true}
+			if(car.model.toLowerCase().search(sbVal) > -1){found = true}
+			if(car.year.search(sbVal) > -1){found = true}
+			hide[i] = found;
+		};
+		return hide;
+	};
+
+	function removeAll(){
+		var tblRow = $("tr");
+		for (var i = 0; i < tblRow.length; i++) {
+			removeHide(tblRow[i]);
+		}
+	};
+
+	function removeHide(tr){
+			if($(tr).hasClass("hidden")){$(tr).removeClass("hidden")};
+	};
 };
